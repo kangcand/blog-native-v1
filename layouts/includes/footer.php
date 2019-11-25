31a1b8b9624ee227ac0aa331a1a750dfab63dd56
Candra <candraherdiansyah14@gmail.com>
2019-11-25 11:36:57 +0700

    Struktur Kode

---
+++ layouts/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="app-footer">
+    <div>
+        <a href="#">Blog-V1</a>
+        <span>&copy; 2019 SMK ASSALAAM</span>
+    </div>
+    <div class="ml-auto">
+        <span>Powered by</span>
+        <a href="https://coreui.io">CoreUI</a>
+    </div>
+</footer>
